9617becebaebbda707e93cdd8c70b580ed09fb26
Jeremy Chen <convexset@gmail.com>
2016-10-26 17:29:50 +0800

    promiseAll_ObjectEdition

--- src/utilities.js
+++ src/utilities.js
@@ -5,5 +5,20 @@ module.exports = function createUtilities(_) {
 				throw new Error('invalid-resource-specification');
 			}
 		},
+
+		// like Promise.all but supports waiting for keyword arguments by
+		// taking an object whose values are promises and resolving to an object
+		// with the same keys but values replaced with the resolved values
+		//
+		// e.g.:
+		// promiseAll_ObjectEdition({a: Promise.resolve(7)}) --> {a: 7}
+		promiseAll_ObjectEdition: function promiseAllObjectEdition(objectWithPromises) {
+			const keys = Object.keys(objectWithPromises);
+			const promises = keys.map(k => objectWithPromises[k] instanceof Promise ? objectWithPromises[k] : Promise.resolve(objectWithPromises[k]));
+			return Promise.all(promises)
+				.then(results => {
+					return _.object(keys.map((k, idx) => [k, results[idx]]));
+				});
+		},
 	};
 };
